9c2aed21a034a0fddd5a03be5a1c463855dba88a
Riccardo Scudieri <riccardo.scudieri@gmail.com>
2022-09-22 20:50:10 +0200

    Add files via upload

---
+++ CashAlert/Views/LoginPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Newtonsoft.Json;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace CashAlert.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public string WebAPIkey = "AIzaSyBcRSMPmoJfHgbdDPxtr5HGCE7apPfsNTU";
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void signupbutton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIkey));
+                var auth = await authProvider.CreateUserWithEmailAndPasswordAsync(UserNewEmail.Text, UserNewPassword.Text);
+                string gettoken = auth.FirebaseToken;
+                await App.Current.MainPage.DisplayAlert("Attenzione", gettoken, "Ok");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Attenzione", ex.Message, "OK");
+            }
+
+        }
+
+        async void loginbutton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(WebAPIkey));
+            try
+            {
+                var auth = await authProvider.SignInWithEmailAndPasswordAsync(UserLoginEmail.Text, UserLoginPassword.Text);
+                var content = await auth.GetFreshAuthAsync();
+                var serializedcontnet = JsonConvert.SerializeObject(content);
+                Preferences.Set("MyFirebaseRefreshToken", serializedcontnet);
+                await Shell.Current.GoToAsync($"//{nameof(ScreenPage)}");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Attenzione", "Mail o Password errate", "OK");
+            }
+        }
+    }
+}
